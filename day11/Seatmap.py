726c4f86f27dc2a83d492739b0f3fec287e20740
Laura Ojala <laojala+github@gmail.com>
2020-12-23 15:28:23 +0200

    day11, part2

--- day11/Seatmap.py
+++ day11/Seatmap.py
@@ -13,37 +13,54 @@ class Seatmap:
 
     @staticmethod
     def process_file():
-        previous = Seatmap(Seatmap._readInitialFile())
-        new = Seatmap([])
+        initial = Seatmap(Seatmap._readInitialFile())
+        
+        previous_part1 = Seatmap(initial.layout[:])
+        new_part1 = Seatmap([])
+
+        Seatmap._loopSeats(previous_part1, new_part1)
+
+        occupied_part1 = Seatmap.count_occupied(new_part1)
+        print('Occupied, part1:', occupied_part1)
+        assert(occupied_part1 == 2406)
+
+        previous_part2 = Seatmap(initial.layout[:])
+        new_part2 = Seatmap([])
+
+        Seatmap._loopSeats(previous_part2, new_part2, False, 5)
+
+        occupied_part2 = Seatmap.count_occupied(new_part2)
+        print('Occupied, part2:', occupied_part2)
+        assert(occupied_part2 == 2149)
+
+    @staticmethod   
+    def _loopSeats(previous, new, part1=True, treshold=4):
 
         while True:
-            newLayout = Seatmap.countNewSeats(previous.layout)
+            newLayout = Seatmap.countNewSeats(previous.layout, part1, treshold)
             new.setLayout(newLayout)
             if previous.layout == new.layout:
                 break
             else:
                 previous.setLayout(new.layout)
 
-        Seatmap.print_part_1_result(new)
-        
     @staticmethod
-    def print_part_1_result(seatmap):
+    def count_occupied(seatmap):
         occupied = 0
         #count seats in new layout
         for row in seatmap.layout:
             occupied = occupied + row.count("#")
-        assert(occupied == 2406)
-        print('Occupied, part 1:', occupied)
+        return occupied
 
     @staticmethod
-    def countNewSeats(seats_map):
+    def countNewSeats(seats_map, part1=True, treshold=4):
         
         new_seat_map = []
         
         for index_row,row in enumerate(seats_map):
 
             new_row = ''
-            seats_around_spot=''
+            seats_around_spot = ''
 
             for index_spot,spot in enumerate(row):
 
@@ -51,8 +68,12 @@ class Seatmap:
                 if spot == '.':
                     new_row = new_row + '.'
                     continue
+
                 #list seats statuses around the spot:
-                seats_around_spot = Seatmap._getSeatsAround(seats_map, index_row, index_spot)
+                if part1:
+                    seats_around_spot = Seatmap._getSeatsAround(seats_map, index_row, index_spot)
+                else:
+                    seats_around_spot = Seatmap._getDiagonalSeats(seats_map, index_row, index_spot)
 
                 if spot == '.':
                     new_row = new_row + '.'
@@ -65,10 +86,10 @@ class Seatmap:
                     new_row = new_row + 'L'
 
                 #release seat if it has four or more occupied seats around it
-                if spot == '#' and seats_around_spot.count('#') >= 4:
+                if spot == '#' and seats_around_spot.count('#') >= treshold:
                     new_row = new_row + 'L'
 
-                if spot == '#' and seats_around_spot.count('#') < 4:
+                if spot == '#' and seats_around_spot.count('#') < treshold:
                     new_row = new_row + '#'
             
                 if index_spot != len(new_row) - 1:
@@ -78,8 +99,89 @@ class Seatmap:
             new_row = ''
             seats_around_spot.clear()
 
-        return new_seat_map
+        return new_seat_map    
 
+    @staticmethod
+    def _getDiagonalSeats(seat_map, row, char):
+        seats_around = []
+
+        seats_around = []
+        map_length = len(seat_map)
+        row_length = len(seat_map[0])
+
+        #up:
+        up = row
+        while up != 0:
+            up = up - 1            
+            if seat_map[up][char] != '.':
+                seats_around.append(seat_map[up][char])
+                break
+                
+        #up-left:
+        up = row
+        left = char
+        while up != 0 and left != 0:
+            up = up - 1
+            left = left -1
+            if seat_map[up][left] != '.':
+                seats_around.append(seat_map[up][left])
+                break
+        
+        #up-right:
+        up = row
+        right = char
+        while up != 0 and right != row_length - 1:
+            up = up - 1
+            right = right + 1
+            if seat_map[up][right] != '.':
+                seats_around.append(seat_map[up][right])
+                break
+
+        #left:
+        left = char
+        while left != 0:
+            left = left - 1
+            if seat_map[row][left] != '.':
+                seats_around.append(seat_map[row][left])
+                break
+
+        #right:
+        right = char
+        while  right != row_length - 1:
+            right = right + 1
+            if seat_map[row][right] != '.':
+                seats_around.append(seat_map[row][right])
+                break
+        
+        #down:
+        down = row
+        while down != map_length - 1:
+            down = down + 1
+            if seat_map[down][char] != '.':
+                seats_around.append(seat_map[down][char])
+                break
+
+        #down-left
+        down = row
+        left = char
+        while down != map_length - 1 and left != 0:
+            down = down + 1
+            left = left - 1
+            if seat_map[down][left] != '.':
+                seats_around.append(seat_map[down][left])
+                break
+        
+        #down-right
+        down = row
+        right = char
+        while down != map_length - 1 and right != row_length - 1:
+            down = down + 1
+            right = right + 1
+            if seat_map[down][right] != '.':
+                seats_around.append(seat_map[down][right])
+                break
+        
+        return seats_around
 
     @staticmethod
     def _getSeatsAround(seat_map, row, char):
@@ -119,4 +221,3 @@ class Seatmap:
         with open(file_path,'r') as f:
             data = f.read().splitlines() 
         return data
-
