00670bfb81c48cc9aad61f4795d3cd1077f647f2
Laura Ojala <laojala+github@gmail.com>
2020-12-25 12:53:57 +0200

    day6, part2

--- day6/CustomCustoms.py
+++ day6/CustomCustoms.py
@@ -18,7 +18,7 @@ class CustomCustoms:
 
         for index,line in enumerate(all_lines):
             if line != '\n':
-                entry = entry + line.strip('\n')
+                entry = entry + line
             if line == '\n' or index == len(all_lines)-1:
                 data.append(entry)
                 entry = ''
@@ -26,24 +26,33 @@ class CustomCustoms:
         return data
 
     @staticmethod 
-    def _printSumOfUnique(all_answers):
-
+    def _printSumOPart1(all_answers):
         sum = 0
-
         for answer in all_answers:
-
             counted = []
-
             for char in answer:
                 if char in string.ascii_lowercase and char not in counted:
                     sum = sum + 1
                     counted.append(char)
-        
         assert(sum == 6310)
         print('part 1', sum)
 
-        
-
+    @staticmethod 
+    def _printSumOPart2(all_answers):
+        sum = 0
+        for answer in all_answers:
+            answers_for_group = answer.split('\n')
+            if '' in answers_for_group:
+                answers_for_group.remove('')
+            counted = []
+            for item in answers_for_group:
+                for char in item:
+                    if all([char in i for i in answers_for_group]) and char in string.ascii_lowercase and char not in counted:
+                        sum = sum + 1
+                        counted.append(char)
+        assert(sum == 3193)
+        print('part 2', sum)
 
 data = CustomCustoms(CustomCustoms._readGroup())
-CustomCustoms._printSumOfUnique(data.groups)
+CustomCustoms._printSumOPart1(data.groups)
+CustomCustoms._printSumOPart2(data.groups)
