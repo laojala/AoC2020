ca7c932d77b4593b1c84ef2d017b2b47e283b0de
Laura Ojala <laojala+github@gmail.com>
2020-12-27 17:29:29 +0200

    day 8, part2

--- day8/HandheldHalting.py
+++ day8/HandheldHalting.py
@@ -1,8 +1,3 @@
-#nop +0
-#acc +1
-#jmp +4
-
-
 boot_code = []
 
 #read data to list of list. First item in a list is int that shows how many times instruction has been visited
@@ -17,24 +12,99 @@ with open('day8.data','r') as f:
 
 accumulator = 0
 index = 0
+boot_part1 = boot_code[:]
 
 while True:
 
-    if boot_code[index][0] == 1:
+    if boot_part1[index][0] == 1:
         break
     
-    if boot_code[index][1] == 'nop':
-        boot_code[index][0] += 1
+    if boot_part1[index][1] == 'nop':
+        boot_part1[index][0] += 1
         index += 1
     
-    if boot_code[index][1] == 'acc':
-        accumulator += boot_code[index][2]
-        boot_code[index][0] += 1
+    if boot_part1[index][1] == 'acc':
+        accumulator += boot_part1[index][2]
+        boot_part1[index][0] += 1
         index += 1
 
-    if boot_code[index][1] == 'jmp':
-        boot_code[index][0] += 1
-        index += boot_code[index][2]
+    if boot_part1[index][1] == 'jmp':
+        boot_part1[index][0] += 1
+        index += boot_part1[index][2]
 
 print('part1 accumulator:', accumulator)
-assert(accumulator == 2014)
+#assert(accumulator == 2014)
+
+#part2
+
+def resetCode(code):
+
+    newCode = []
+
+    for item in code:
+        if item[0] == 1:
+            item[0] = 0
+        if item[0] == 100:
+            item[0] = 99
+        newCode.append(item)
+    
+    return newCode
+
+
+lenght = len(boot_code)
+solved = False
+accumulator = 0
+
+while not solved:
+
+    if solved:
+        break
+
+    index = 0
+    accumulator = 0
+    operation_changed = False #one operation changed on a round
+    boot_code = resetCode(boot_code)
+
+    while True:
+
+        if(index == lenght):
+            solved = True
+            break
+
+        visited = boot_code[index][0]
+        operation = boot_code[index][1]
+        argument = boot_code[index][2]
+
+        if(visited == 1 or visited == 100):
+            operation_changed = False
+            break
+
+        if operation == 'acc':
+            accumulator += argument
+            boot_code[index][0] += 1
+            index += 1
+
+        if operation == 'nop':
+            #if not visited and not changed for this round, change operation:
+            if visited == 0 and not operation_changed:
+                boot_code[index][0] = 99
+                operation_changed = True
+                index += argument  #do jmp
+            #otherwise do nop
+            else:
+                boot_code[index][0] += 1
+                index += 1
+        
+        if operation == 'jmp':
+            #if not visited and not changed in this round, change operation:
+            if visited == 0 and not operation_changed:
+                boot_code[index][0] = 99
+                operation_changed = True
+                index += 1  #do nop
+            #otherwise do jmp
+            else:
+                boot_code[index][0] += 1
+                index += argument
+
+print('part2 accumulator:', accumulator)
+assert(accumulator == 2251)
