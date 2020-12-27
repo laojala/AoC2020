7614fc67ae90ae7401b371472f1d02e2e908d03a
Laura Ojala <laojala+github@gmail.com>
2020-12-27 20:41:55 +0200

    day9, part2

--- day9/EncodingError.py
+++ day9/EncodingError.py
@@ -2,6 +2,7 @@ with open('day9.data','r') as f:
     data = f.read().splitlines()
     data = [int(i) for i in data]
 
+
 preamble = 25
 number = None
 
@@ -24,3 +25,28 @@ for i in range(preamble, len(data)):
         print('part1:', number)
         assert(number == 22406676)
         break  
+
+#part2
+list_found = False
+
+for index , n in enumerate(data):
+    
+    sum = 0
+
+    if list_found:
+        print('part2:', min(list) + max(list))
+        assert(min(list) + max(list) == 2942387)
+        break
+
+    list = []
+
+    for i in range(index+1, len(data)-1):
+        sum += data[i]
+        list.append(data[i])
+
+        if sum == number and len(list) >= 2:
+            list_found = True
+            break
+    
+        if sum > number:
+            break
